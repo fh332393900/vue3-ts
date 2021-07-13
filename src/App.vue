5c445075bffa4baea97b401a90c283a810a6dad1
fenghang <332393900@qq.com>
2021-07-13 17:59:36 +0800

    修改字体

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,16 @@ body {
   height: 100%;
   margin: 0;
   padding: 0;
+	font-family: 'PingFang SC',-apple-system, AdobeHeitiStd-Regular, BlinkMacSystemFont,
+    'PingFang SC', 'Helvetica Neue', STHeiti, 'Microsoft Yahei', Tahoma, Simsun,
+    sans-serif;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
   color: #2c3e50;
+	font-size: 14px;
   a {
     text-decoration: none;
   }
